59e8d1ceb23c1fda09a0f19cb752cd31a269e14a
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-12-07 05:30:33 -0500

    Combine multiple tables

--- main.js
+++ main.js
@@ -19,14 +19,11 @@ function invalidKey() {
   }
 }
 
-function resultsAsDatasets(results, asctbAPI, versions) {
-  console.log(results)
-  console.log(asctbAPI)
-  console.log(versions)
+function resultsAsDatasets(jsonData, asctbAPI, versions) {
   const sheetOptions = versions.sheetOptions;
   const nodes = asctbAPI.data.nodes;
   const csvItems = [];
-  results.tissueInfo.forEach(tissueInfo => {
+  jsonData.tissueInfo.forEach(tissueInfo => {
     const uberonId = tissueInfo.uberonId;
     const match = nodes.find(node => node.metadata.ontologyId === `UBERON:${uberonId}`);
     const matchingEntry = sheetOptions.find(entry => entry.title === tissueInfo.tissueSite);
@@ -55,29 +52,74 @@ function downloadTable() {
   }
 }
 
+function getOrganUris(config) {
+  const organUris = [];
+  for (const organ of config.sheetDetails) {
+    if (organ.version) {
+      for (const version of organ.version) {
+        let asctbAPIUri = `https://asctb-api.herokuapp.com/v2/csv?csvUrl=https://docs.google.com/spreadsheets/d/${version.sheetId}/export?format=csv%26gid=${version.gid}&output=graph`;
+        organUris.push(asctbAPIUri)
+      }
+    }
+  }
+  return organUris;
+}
+
+function matchingData(result, csvItems) {
+  let matches = [];
+  matches = csvItems.filter(item => item['ASCT+B Table'] !== 'No matches')
+  return matches
+}
+
 function main() {
   let searchUri = 'https://gtexportal.org/rest/v1/dataset/tissueInfo?datasetId=gtex_v8&format=json';
-  // let asctbAPIUri = 'https://asctb-api.herokuapp.com/v2/csv?csvUrl=https://docs.google.com/spreadsheets/d/1tK916JyG5ZSXW_cXfsyZnzXfjyoN-8B2GXLbYD6_vF0/export?format=csv%26gid=2137043090&output=graph';
-  let organUris = [];
 
   Promise.all([
     fetch("https://hubmapconsortium.github.io/ccf-gtex-data-dashboard/vis.vl.json").then((result) => result.json()),
     fetch(searchUri).then((result) => result.ok ? result.json() : invalidKey()),
-    // fetch(asctbAPIUri).then((result) => result.ok ? result.json() : invalidKey()),
     fetch("https://hubmapconsortium.github.io/ccf-gtex-data-dashboard/sheet-config.json").then((result) => result.json()),
     fetch("https://hubmapconsortium.github.io/ccf-gtex-data-dashboard/versions.json").then((result) => result.json()),
   ]).then(([spec, jsonData, config, versions]) => {
-    console.log(config)
-    // Embed the graph data in the spec for ease of use from Vega Editor
-    spec.datasets = resultsAsDatasets(jsonData, asctbAPI, versions);
-    table = new Tabulator("#table", {
-      data: spec.datasets.csvItems,
-      autoColumns: true
+    const organUris = getOrganUris(config)
+    let x = [];
+    for (const uri of organUris) {
+      x.push(fetch(uri).then((result) => result.ok ? result.json() : invalidKey()))
+    }
+    
+    Promise.all(x).then((allSets) => {
+      console.log(allSets)
+      let result = [];
+      let seen = new Set;
+      for (const dataset of allSets) {
+        console.log(result)
+        const csvItems = resultsAsDatasets(jsonData, dataset, versions).csvItems
+        console.log(matchingData(result, csvItems))
+        const matches = matchingData(result, csvItems)
+        if (matches.length > 0) {
+          console.log(seen)
+          console.log(matches[0]['GTEx Ontology ID'])
+          if (!seen.has(matches[0]['GTEx Ontology ID'])) {
+            result = result.concat(matches)
+            seen.add(matches[0]['GTEx Ontology ID'])
+            console.log(result)
+            console.log(seen)
+          }
+        }
+        result = result.concat(csvItems)
+      }
+      let unique = [...new Set(result)];
+      unique.sort((a, b) => (a['ASCT+B Table'] === 'No matches') ? 1 : -1)
+      return unique
+    }).then((result) => {
+      // console.log(result.slice(10))
+      table = new Tabulator("#table", {
+        data: result,
+        autoColumns: true
+      });
+      // Embed the graph data in the spec for ease of use from Vega Editor
+      return vegaEmbed("#visualization", spec, { "renderer": "svg", "actions": true });
+    }).then((results) => {
+      console.log("Visualization successfully loaded");
     });
-
-    return vegaEmbed("#visualization", spec, { "renderer": "svg", "actions": true });
-  }).then((results) => {
-    console.log("Visualization successfully loaded");
-  });
-}
+  })}
 main();
