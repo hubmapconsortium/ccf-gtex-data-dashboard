ff41a96cf1bd042a6113c3d41fcef6bc100372af
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-12-07 11:56:36 -0500

    Fix table

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ function resultsAsDatasets(jsonData, asctbAPI, versions) {
     const matchingEntry = sheetOptions.find(entry => entry.title === tissueInfo.tissueSite);
     const csvItem = {
       "GTEx Ontology ID": `UBERON:${uberonId}`,
-      "GTEx Tissue Site": tissueInfo.tissueSite || 'No matches',
+      "GTEx Tissue Site": tissueInfo.tissueSiteDetail || 'No matches',
       "Ontology term": match ? match.metadata.label : 'No matches',
       "ASCT+B Table": 'No matches'
     };
     if (matchingEntry && match) {
       for (const version of matchingEntry.version) {
-        csvItems.push({ ...csvItem, "ASCT+B Table": version.value })
+        csvItems.push({ ...csvItem, "ASCT+B Table": version.value });
       }
     } else {
       csvItems.push(csvItem);
@@ -58,60 +58,68 @@ function getOrganUris(config) {
     if (organ.version) {
       for (const version of organ.version) {
         let asctbAPIUri = `https://asctb-api.herokuapp.com/v2/csv?csvUrl=https://docs.google.com/spreadsheets/d/${version.sheetId}/export?format=csv%26gid=${version.gid}&output=graph`;
-        organUris.push(asctbAPIUri)
+        organUris.push(asctbAPIUri);
       }
     }
   }
   return organUris;
 }
 
-function matchingData(result, csvItems) {
+function matchingData(csvItems) {
   let matches = [];
-  matches = csvItems.filter(item => item['ASCT+B Table'] !== 'No matches')
-  return matches
+  matches = csvItems.filter(item => item['ASCT+B Table'] !== 'No matches');
+  return matches;
+}
+
+function unmatchingData(csvItems) {
+  let unmatches = [];
+  unmatches = csvItems.filter(item => item['ASCT+B Table'] === 'No matches');
+  return unmatches;
+}
+
+function addMatches(result, matches) {
+  const ontologyId = matches[0]['GTEx Ontology ID'];
+  result = result.filter(item => item['GTEx Ontology ID'] !== ontologyId);
+  return result.concat(matches);
 }
 
 function main() {
   let searchUri = 'https://gtexportal.org/rest/v1/dataset/tissueInfo?datasetId=gtex_v8&format=json';
 
   Promise.all([
-    fetch("https://hubmapconsortium.github.io/ccf-gtex-data-dashboard/vis.vl.json").then((result) => result.json()),
+    fetch("vis.vl.json").then((result) => result.json()),
     fetch(searchUri).then((result) => result.ok ? result.json() : invalidKey()),
-    fetch("https://hubmapconsortium.github.io/ccf-gtex-data-dashboard/sheet-config.json").then((result) => result.json()),
-    fetch("https://hubmapconsortium.github.io/ccf-gtex-data-dashboard/versions.json").then((result) => result.json()),
+    fetch("sheet-config.json").then((result) => result.json()),
+    fetch("versions.json").then((result) => result.json()),
   ]).then(([spec, jsonData, config, versions]) => {
-    const organUris = getOrganUris(config)
+    const organUris = getOrganUris(config);
     let x = [];
     for (const uri of organUris) {
-      x.push(fetch(uri).then((result) => result.ok ? result.json() : invalidKey()))
+      x.push(fetch(uri).then((result) => result.ok ? result.json() : invalidKey()));
     }
     
     Promise.all(x).then((allSets) => {
-      console.log(allSets)
       let result = [];
       let seen = new Set;
       for (const dataset of allSets) {
-        console.log(result)
-        const csvItems = resultsAsDatasets(jsonData, dataset, versions).csvItems
-        console.log(matchingData(result, csvItems))
-        const matches = matchingData(result, csvItems)
+        const csvItems = resultsAsDatasets(jsonData, dataset, versions).csvItems;
+        const matches = matchingData(csvItems);
+        const unmatches = unmatchingData(csvItems);
+        if (result.length === 0) {
+          result = result.concat(unmatches);
+        }
         if (matches.length > 0) {
-          console.log(seen)
-          console.log(matches[0]['GTEx Ontology ID'])
-          if (!seen.has(matches[0]['GTEx Ontology ID'])) {
-            result = result.concat(matches)
-            seen.add(matches[0]['GTEx Ontology ID'])
-            console.log(result)
-            console.log(seen)
+          const ontologyId = matches[0]['GTEx Ontology ID'];
+          if (!seen.has(ontologyId)) {
+            result = addMatches(result, matches);
+            seen.add(ontologyId);
           }
         }
-        result = result.concat(csvItems)
       }
       let unique = [...new Set(result)];
-      unique.sort((a, b) => (a['ASCT+B Table'] === 'No matches') ? 1 : -1)
-      return unique
+      unique.sort((a, b) => (a['ASCT+B Table'] === 'No matches') ? 1 : -1);
+      return unique;
     }).then((result) => {
-      // console.log(result.slice(10))
       table = new Tabulator("#table", {
         data: result,
         autoColumns: true
